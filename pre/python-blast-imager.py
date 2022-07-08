e8e63934724e10a267906e472a6b738c00b89ae1
mitsuharu sato <mitsuharu@debian-BULLSEYE-live-builder-AMD64>
2022-07-08 18:43:17 +0800

    end of Jul08

--- pre/python-blast-imager.py
+++ pre/python-blast-imager.py
@@ -3,6 +3,8 @@
 
 import sys
 from collections import defaultdict
+from PIL import Image, ImageDraw, ImageFont
+from matplotlib import font_manager
 
 
 def fmt6parser(fmt6):
@@ -40,6 +42,9 @@ def fmt6parser(fmt6):
     sids = []  # subject ids in order
     hsp_bucket = defaultdict(list)
     hsp_count = 0
+    # for query start min and query end max in hsp
+    qs_min = 1e20
+    qe_max = 0
 
     with open(fmt6, mode='r') as FMT6:
         for line in FMT6:
@@ -47,26 +52,46 @@ def fmt6parser(fmt6):
             # get what i want
             qid = parts[col_labels.index('qseqid')]
             sid = parts[col_labels.index('sseqid')]
-            p_i = parts[col_labels.index('pidebt')]
-            q_s = parts[col_labels.index('qstart')]
-            q_e = parts[col_labels.index('qend')]
-            s_s = parts[col_labels.index('sstart')]
-            s_e = parts[col_labels.index('semd')]
+            p_i = float(parts[col_labels.index('pident')])
+            q_s = int(parts[col_labels.index('qstart')])
+            q_e = int(parts[col_labels.index('qend')])
+            s_s = int(parts[col_labels.index('sstart')])
+            s_e = int(parts[col_labels.index('send')])
             # populate buckets
             if sid not in hsp_bucket:
                 # `not in sids` will do the same.  I followed original pl code.
                 sids.append(sid)
-            hsp_bucket[sid].append((q_s, q_e, s_s, s_e, p_i))
+            hsp_bucket[sid].append((q_s, q_e, s_s, s_e, p_i))  # all in str
             hsp_count += 1
+            # min, max
+            if q_s < qs_min:
+                qs_min = q_s
+            if q_e > qe_max:
+                qe_max = q_e
 
-    return qid, sids, hsp_bucket, hsp_count
+    return qid, sids, hsp_bucket, hsp_count, qs_min, qe_max
+
+
+def colormap(percentage, colors):
+    if percentage >= 100:
+        idx = 0
+    else:
+        idx = int((109 - percentage) / 10)
+
+    return colors[idx]
+
+
+def scale(x, qs_min, scale, left):
+    scale = (x - qs_min) * scale + left
+    return scale
 
 
 def mainStory():
     # get fmt6 file path
     fmt6 = sys.argv[1]
     # parse hit info from fmt6
-    query, sids, hsp_bucket, hsp_count = fmt6parser(fmt6)
+    query, sids, hsp_bucket, hsp_count, qs_min, qe_max = fmt6parser(fmt6)
+
     # graph parameter setup
     LEFT, BODY, RIGHT = (150, 550, 50)
     HEADER_H, FOOTER_H = (40, 20)
@@ -78,20 +103,101 @@ def mainStory():
         v_size = 100
     if v_size > 4000:  # ceiling
         v_size = 4000
-    h_size = sum(LEFT, BODY, RIGHT)
-    q_s_min = min([t[0] for t in hsp_bucket.values()])
-    q_e_max = max([t[1] for t in hsp_bucket.values()])
-    scale = BODY / (q_e_max - q_s_min + 1)
+    h_size = sum((LEFT, BODY, RIGHT))
+    SCALE = BODY / (qe_max - qs_min + 1)
     # colors
-    
+    colors = (
+        (0, 0, 0),  # black
+        (196, 0, 255), (0, 0, 255), (0, 255, 255), (0, 255, 0),
+        (255, 255, 0), (255, 196, 0), (255, 0, 0), (128, 128, 128),
+        (255, 255, 255)  # white
+    )
+
+    # creating ImageDraw object
+    img = Image.new("RGB", (h_size, v_size), colors[-1])  # white is the last
+    draw = ImageDraw.Draw(img)
+    # fonts
+    font = font_manager.FontProperties(family='monospace')
+    font_b = font_manager.FontProperties(family='monospace', weight='bold')
+    font_file = font_manager.findfont(font)
+    font_file_b = font_manager.findfont(font_b)
+
+    font_tiny = ImageFont.truetype(font_file, size=8)
+    font_small = ImageFont.truetype(font_file, size=10)
+    font_medium_b = ImageFont.truetype(font_file_b, size=12)
 
     # header part
-    # percent identity
+    # query name
+    draw.text(
+        (5, HEADER_H - 8), query[:18], font=font_medium_b, fill=colors[0]
+    )
+    # query as a line
+    draw.line([(LEFT, HEADER_H), (LEFT + BODY, HEADER_H)], fill=colors[0])
+    # query hit start and end
+    draw.text(
+        (LEFT, HEADER_H - 20), str(qs_min),
+        font=font_small, fill=colors[0]
+    )
+    draw.text(
+        (LEFT + BODY, HEADER_H - 20), str(qe_max),
+        font=font_small, fill=colors[0]
+    )
+
+    # percent identity color scale
+    draw.text((670, 5), "% Identity", font=font_small, fill=colors[0])
+    # color belt
+    for perc in range(20, 101, 10):
+        x = LEFT + BODY/2 + perc*2
+        draw.rectangle([(x, 5), (x + 10, 15)], fill=colormap(perc, colors))
+        draw.text((x, 17), str(perc), font=font_tiny, fill=colors[0])
+
     # alignments
+    depth = defaultdict(int)
+    v = 0
+    for sid in sids:
+        v += SUBJ_SEP
+        draw.text(
+            (10, HEADER_H + v + 9), sid[:18], font=font_small, fill=colors[0]
+        )
+        for hsp in hsp_bucket[sid]:
+            v += HSP_SEP
+            (q_s, q_e, s_s, s_e, p_i) = hsp
+            if s_s < s_e:
+                strand = '+'
+            else:
+                strand = '-'
+            # map start end location in the drawing
+            x1 = scale(q_s, qs_min, SCALE, LEFT)
+            x2 = scale(q_e, qs_min, SCALE, LEFT)
+            print(x1, x2)
+            y = HEADER_H + v
+            # for x in range(x1, x2 + 1):
+            #     depth[x] += 1
+            # draw hsp line, location on both end
+            draw.rectangle(
+                [(x1, y), (x2, y + LINE_W)], fill=colormap(p_i, colors)
+            )
+            draw.text(
+                (x1 - 5*len(str(q_s)), y - 5), str(q_s),
+                font=font_tiny, fill=colors[0]
+            )
+            draw.text(
+                (x2 + 2, y - 5), str(q_e),
+                font=font_tiny, fill=colors[0]
+            )
+            draw.text(
+                (x1 - 5*len(str(s_s)), y + 2), str(s_s),
+                font=font_tiny, fill=colors[0]
+            )
+            draw.text(
+                (x2 + 2, y + 2), str(s_e) + ' ' + strand,
+                font=font_tiny, fill=colors[0]
+            )
+
     # alignments depth in the header
 
 
-
+    img.save('py-b-imgr-test.png')
 
 
 
